45fdfa76b95c19f2a993bde0070d8a97b0e3c351
Satoru Noguchi <satoru.nog@gmail.com>
2019-06-08 11:03:38 -0300

    aula 1

--- cdc-admin/src/index.js
+++ cdc-admin/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/pure-min.css';
+import './css/side-menu.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
