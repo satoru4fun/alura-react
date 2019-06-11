9ea9ae54805748650e32074e91554b17acc30549
Satoru Noguchi <satoru.nog@gmail.com>
2019-06-11 20:17:48 -0300

    aula 5

--- cdc-admin/src/App.js
+++ cdc-admin/src/App.js
@@ -1,41 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import $ from 'jquery';
+
+import AutorBox from './Autor';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {lista: []};
-  }
-
-  componentDidMount(){
-    $.ajax({
-        url:"http://cdc-react.herokuapp.com/api/autores",
-        dataType: 'json',
-        success:function(resposta){
-          this.setState({lista:resposta});
-          }.bind(this)
-    })
-  }
-
-  enviaForm(evento) {
-    evento.preventDefault();
-
-    $.ajax({
-      url: "http://cdc-react.herokuapp.com/api/autores",
-      contentType: 'application/json',
-      dataType: 'json',
-      type: 'post',
-      data: JSON.stringify({nome:'', email:'', senha:''}),
-      success: function(resposta) {
-        console.log("sucesso");
-      },
-      error: function(resposta) {
-        console.log("erro");
-      }
-    });
-  }
-
+  
   render() {
 
     return (
@@ -59,49 +28,7 @@ class App extends Component {
                 <h1>Cadastro de Autores</h1>
               </div>
               <div className="content" id="content">
-                <div className="pure-form pure-form-aligned">
-                  <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
-                    <div className="pure-control-group">
-                      <label htmlFor="nome">Nome</label> 
-                      <input id="nome" type="text" name="nome" value=""  />                  
-                    </div>
-                    <div className="pure-control-group">
-                      <label htmlFor="email">Email</label> 
-                      <input id="email" type="email" name="email" value=""  />                  
-                    </div>
-                    <div className="pure-control-group">
-                      <label htmlFor="senha">Senha</label> 
-                      <input id="senha" type="password" name="senha"  />                                      
-                    </div>
-                    <div className="pure-control-group">                                  
-                      <label></label> 
-                      <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
-                    </div>
-                  </form>             
-  
-                </div>  
-                <div>            
-                  <table className="pure-table">
-                    <thead>
-                      <tr>
-                        <th>Nome</th>
-                        <th>email</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {
-                        this.state.lista.map(function(autor) {
-                          return (
-                            <tr key={autor.id}>
-                              <td>{autor.nome}</td>
-                              <td>{autor.email}</td>
-                            </tr>
-                          );
-                        })
-                      }
-                    </tbody>
-                  </table> 
-                </div>             
+                <AutorBox/>
               </div>
             </div>
       </div>
---
+++ cdc-admin/src/Autor.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+import InputCustomizado from './components/InputCustomizado';
+import SubmitCustomizado from './components/SubmitCustomizado';
+
+class FormularioAutor extends Component {
+    constructor() {
+        super();
+        this.state = {nome:'', email:'', senha:''};
+        this.enviaForm = this.enviaForm.bind(this);
+        this.setNome = this.setNome.bind(this);
+        this.setEmail = this.setEmail.bind(this);
+        this.setSenha = this.setSenha.bind(this);
+    }
+
+    enviaForm(evento) {
+        evento.preventDefault();
+    
+        $.ajax({
+          url: "http://cdc-react.herokuapp.com/api/autores",
+          contentType: 'application/json',
+          dataType: 'json',
+          type: 'post',
+          data: JSON.stringify({nome:this.state.nome, email:this.state.email, senha:this.state.senha}),
+          success: function(resposta) {
+            this.props.callBackListagem(resposta);
+          }.bind(this),
+          error: function(resposta) {
+            console.log("erro");
+          }
+        });
+      }
+
+    setNome(evento) {
+        this.setState({nome:evento.target.value});
+    }
+    
+    setEmail(evento) {
+        this.setState({email:evento.target.value});
+    }
+
+    setSenha(evento) {
+        this.setState({senha:evento.target.value});
+    }
+
+    render() {
+        return(
+            <div className="pure-form pure-form-aligned">
+                <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
+                    <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome"/>
+                    <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email"/>
+                    <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha"/>
+                    <SubmitCustomizado label="Gravar"/>
+                </form>
+            </div>
+        );
+    }
+}
+
+class ListaAutores extends Component {
+    render() {
+        return(
+            <div>
+                <table className="pure-table">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.props.lista.map(function(autor) {
+                                return (
+                                    <tr key={autor.id}>
+                                        <td>{autor.nome}</td>
+                                        <td>{autor.email}</td>
+                                    </tr>
+                                );
+                            })
+                        }
+                    </tbody>
+                </table> 
+            </div>     
+        );
+    }
+}
+
+export default class AutorBox extends Component {
+    constructor() {
+        super();
+        this.state = {lista: []};
+        this.callBackListagem = this.callBackListagem.bind(this);
+    }
+    
+    componentDidMount(){
+        $.ajax({
+            url:"http://cdc-react.herokuapp.com/api/autores",
+            dataType: 'json',
+            success:function(resposta){
+                this.setState({lista:resposta});
+            }.bind(this)
+        })
+    }
+
+    callBackListagem(novaLista) {
+        this.setState({lista: novaLista});
+    }
+    
+    render() {
+        return(
+            <div>
+                <FormularioAutor callBackListagem={this.callBackListagem}/>
+                <ListaAutores lista={this.state.lista}/>
+            </div>
+        );
+    }
+}
